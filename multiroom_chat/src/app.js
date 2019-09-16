88b14f57840985c8c1ce543f045e51258baa9b2d
Wesley Campagna <wesley.campagna@hotmail.com>
2019-09-15 22:01:52 -0400

    Finalizado modulo de webSockets

--- multiroom_chat/src/app.js
+++ multiroom_chat/src/app.js
@@ -1,6 +1,30 @@
 const app = require('../config/env')
 
-app.listen (8080, () => {
+const server = app.listen (8080, () => {
     console.log('listen at port 8080')
+})
+
+const io = require('socket.io').listen(server)
+
+app.set('io', io) // interessante: a funtion set serve para atribuir global objects dentro do express
+
+// criar a conexão por websocket
+io.on('connection', (socket) => {
+    console.log('user connected!')
+
+    //socket.emit('disconnected', {apelido: 'jao'})
+    socket.on('disconnect',  (socket) => {
+        console.log('user disconnect!')
+    })
+
+    socket.on('msg-server', (data) => {
+        socket.emit('msgToClient', {apelido: data.apelido, msg: data.msg})
+        socket.broadcast.emit('msgToClient', {apelido: data.apelido, msg: data.msg})
+
+        if (parseInt(data.hasConnected) === 0) {
+            socket.emit('toAll', {apelido: data.apelido})
+            socket.broadcast.emit('toAll', {apelido: data.apelido})
+        }
+    })
 })
 
--- multiroom_chat/src/controllers/chat.js
+++ multiroom_chat/src/controllers/chat.js
@@ -1,3 +1,10 @@
 module.exports.chat = (app, req, res) => {
-    res.render('chat')
+
+    formData = req.body
+    console.log(formData)
+    
+    /* validação do campo como não vazio e tamanho de valores entrados  */
+    app.get('io').emit('clientConnect', {apelido: formData.apelido, msg: null}) // app.get recupera objetos globais
+    
+    res.render('chat', {dataForm : formData})
 }
